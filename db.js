4b0787d65300697d184eb2ae3792a3ed05f26cc7
JonasJamilano <141260810+JonasJamilano@users.noreply.github.com>
2026-02-16 21:25:54 +0800

    SQL Icons

--- db.js
+++ db.js
@@ -3,6 +3,6 @@ import mysql from "mysql2/promise";
 export const db = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "Ilovedota2valorant!?", // put your MySQL password
+  password: "StephenCurry30!", // put your MySQL password
   database: "aquafresh_db"
 });
