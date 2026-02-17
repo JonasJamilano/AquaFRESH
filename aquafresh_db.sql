36cb6854e1893c7c2ef77320e82bc54597c4eb20
jcsilvaaa <162688556+jcsilvaaa@users.noreply.github.com>
2026-02-18 02:43:36 +0800

    Update aquafresh_db.sql

--- aquafresh_db.sql
+++ aquafresh_db.sql
@@ -1,4 +1,11 @@
-CREATE TABLE users (
+-- Create Database
+CREATE DATABASE IF NOT EXISTS aquafresh_db;
+USE aquafresh_db;
+
+
+-- USERS TABLE
+
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     full_name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
@@ -8,4 +15,47 @@ CREATE TABLE users (
     role ENUM('superadmin','admin','manager','inspector','delivery') NOT NULL,
     status ENUM('active','inactive') DEFAULT 'active',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+-- QUALITY CONTROL TABLES
+
+
+CREATE TABLE IF NOT EXISTS inspection_batches (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    batch_code VARCHAR(50) NOT NULL UNIQUE,
+    product_type VARCHAR(100) NOT NULL,
+    location VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE IF NOT EXISTS inspection_logs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    batch_id INT NOT NULL,
+    inspector_id INT NOT NULL,
+    overall_status ENUM('Passed','With Issues','Rejected') NOT NULL,
+    overall_remarks TEXT,
+    inspection_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (batch_id) 
+        REFERENCES inspection_batches(id) 
+        ON DELETE CASCADE,
+
+    FOREIGN KEY (inspector_id) 
+        REFERENCES users(id) 
+        ON DELETE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS inspection_criteria_results (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    inspection_log_id INT NOT NULL,
+    criteria_name VARCHAR(100) NOT NULL,
+    assessment ENUM('Excellent','Acceptable','Rejected') NOT NULL,
+    remarks TEXT,
+
+    FOREIGN KEY (inspection_log_id) 
+        REFERENCES inspection_logs(id) 
+        ON DELETE CASCADE
 );
